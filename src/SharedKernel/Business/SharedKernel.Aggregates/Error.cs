41b67e89ab3a99167b41759251dfad8b177746ba
Andrey Moshneaga <symasoiti97@gmail.com>
2024-07-29 07:16:04 +0300

    Remove Thrower.cs
    The code are not used

--- src/SharedKernel/Business/SharedKernel.Aggregates/Error.cs
+++ src/SharedKernel/Business/SharedKernel.Aggregates/Error.cs
@@ -10,15 +10,6 @@ public abstract class Error
     /// </summary>
     public abstract string Message { get; }
 
-    /// <summary>
-    /// Инициализирует <see cref="Thrower" />
-    /// </summary>
-    /// <returns></returns>
-    public static Thrower Throw()
-    {
-        return new Thrower();
-    }
-
     public static implicit operator ErrorException(Error error) => new(error, message: null);
 
     public ErrorException WithDetails(string details) => new(this, details);
